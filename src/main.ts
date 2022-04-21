b7a9139f503f243744ce8820c76c92cc8ea995f5
hgjoka <hegi.gjoka@eng.it>
2022-04-21 15:18:28 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { environment } from './environment/environment';
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix(environment.server.prefix);
   app.useGlobalPipes(new ValidationPipe())
 
